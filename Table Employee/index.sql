d939fac0837977bca13f8af12fd8c7c0a5cee369
Novani Indra Kustanti <82430501+novaniindra@users.noreply.github.com>
2021-09-25 11:17:11 +0700

    Update index.sql

--- Table Employee/index.sql
+++ Table Employee/index.sql
@@ -1,1 +1,3 @@
-
+# Understand The Database and Access The Employee Tables
+In my first repository, I will understand the database and access the Employee Tables using PostgreSQL with DBeaver.
+The Data I got from ShiftAcademy.
